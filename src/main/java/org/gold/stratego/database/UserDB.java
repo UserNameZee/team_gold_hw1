9faa739a54ea485ee26e7c4c11e3b3bd0c816a43
Jacob <jathoma98@gmail.com>
2019-09-18 16:57:07 -0400

    Added a user authentication method to UserDB, along with tests.

--- src/main/java/org/gold/stratego/database/UserDB.java
+++ src/main/java/org/gold/stratego/database/UserDB.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.security.MessageDigest;
+import java.util.Arrays;
 
 /**
  * Wrapper for Spring's UserRepository
@@ -13,17 +15,25 @@ import java.util.List;
 @Transactional
 @Component
 public class UserDB{
+    /**
+     * Enum used to check the result of a login attempt in method authenticateUser()
+     */
+    public enum UserAuthenticationStatus
+    {
+        USERNAME_NOT_FOUND,
+        INVALID_PASSWORD,
+        SUCCESSFUL;
+    }
 
     @Autowired
     private UserRepository userRepository;
 
     public void insertUser(String username, String password){
-        //TODO: hash the password
         User user = new User();
         user.setUsername(username);
-        user.setPass_hash(password);
-
+        user.setPass_hash(getHash(password));
         userRepository.save(user);
+
     }
 
     public User findUser(String username){
@@ -39,6 +49,41 @@ public class UserDB{
 
     public void deleteUser(String username){
         userRepository.deleteByUsername(username);
+    }
+
+    /**
+     * Attempts to authenticate the given username and password
+     * @param username
+     * @param password
+     * @return - One of three statuses (defined above as enum):
+     *          USERNAME_NOT_FOUND - The username is not present in the database.
+     *          INVALID_PASSWORD - Username was found, but password does not match.
+     *          SUCCESSFUL - Username and password exist in the DB.
+     */
+    public UserAuthenticationStatus authenticateUser(String username, String password){
+        User found = findUser(username);
+        if (found == null)
+            return UserAuthenticationStatus.USERNAME_NOT_FOUND;
+        byte[] db_hash = found.getPass_hash();
+        byte[] user_hash = getHash(password);
+        if (!Arrays.equals(db_hash, user_hash))
+            return UserAuthenticationStatus.INVALID_PASSWORD;
+        return UserAuthenticationStatus.SUCCESSFUL;
+
+    }
+
+    /**
+     * Hashes the given string.
+     */
+    public byte[] getHash(String entry){
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(entry.getBytes());
+            return md.digest();
+        } catch (Exception e){
+            System.out.println("Exception occurred while hashing password.");
+            return null;
+        }
     }
 
     /**
