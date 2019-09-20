eefb1b7b9c371bae10865138bc7cc2e549d33763
peilin zhu <zpl969129933@gmail.com>
2019-09-21 08:32:54 +0900

    peilin zhu -info controller

--- src/main/java/org/gold/stratego/controller/InfoController.java
+++ src/main/java/org/gold/stratego/controller/InfoController.java
@@ -12,10 +12,17 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping(value = "/return")
 public class InfoController {
-
+    /**
+     * Model本身不能设置页面跳转的url地址别名或者物理跳转地址，那么我们可以通过控制器方法的返回值来设置跳转url
+     * 地址别名或者物理跳转地址
+     *
+     * @param model
+     *            一个接口， 其实现类为ExtendedModelMap，继承了ModelMap类
+     * @return 跳转url地址别名或者物理跳转地址
+     */
     @RequestMapping(value = "/login.html")
     public String index1(Model model) {
         model.addAttribute("result", "后台返回index1");
         return "result";
-
+    }
 }
