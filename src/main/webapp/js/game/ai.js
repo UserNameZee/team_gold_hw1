e2663dab8488b5d72dca4ddfa04feaa7d767ac1a
peilin zhu <zpl969129933@gmail.com>
2019-09-24 05:50:55 +0900

    peilin zhu- AI

--- src/main/webapp/js/game/ai.js
+++ src/main/webapp/js/game/ai.js
@@ -8,5 +8,43 @@ class AI{
         setTimeout(function (){
             s.switchTurn();
         }, 5000);
+        return true;
     }
+    aiMove(){
+        this.findMovablePieces(this.stratego.chessBoardData,this.stratego.team1)
+
+    }
+
+    /*
+    *  GetBoard, get player move
+    * */
+    GetAIMove(playermove,board){
+
+
+
+    }
+    makeGuesses(){
+        if( this.fakeMove()){
+        }
+    }
+
+    getAllMove(start,board) {
+        let row = start[0];
+        let col = start[1];
+        let mypieces = board[row][col];
+        let myRank = this.stratego.team1;
+    }
+
+    /*寻找所有棋子，可移动棋子加入array */
+    findMovablePieces(board,AI){
+        for (let y = 0 ; y < AI.length; y++){
+            let x_length = AI.length[y];
+            for(let x = 0 ; x < x_length.length; x++){
+                if()
+            }
+        }
+    }
+
+
+
 }
