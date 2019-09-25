04896fa586d428e63ca5eea5ad184d32599542ed
Jacob <jathoma98@gmail.com>
2019-09-25 17:40:53 -0400

    Allowed unauthorized users access to static files (img, .js, css)

--- src/main/java/org/gold/stratego/security/SecurityConfig.java
+++ src/main/java/org/gold/stratego/security/SecurityConfig.java
@@ -8,6 +8,12 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
+    public void configure(WebSecurity web) throws Exception{
+        web.ignoring().antMatchers("/webapp/style.css").anyRequest();
+
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
