dc06e676740d88bdc5af0fa2e97d345d8c2cd802
Chenxing He <chenhe@cs.stonybrook.edu>
2019-09-25 18:31:28 -0400

    Update ai.js

--- src/main/webapp/static/js/game/ai.js
+++ src/main/webapp/static/js/game/ai.js
@@ -13,9 +13,12 @@ class AI{
 
     aiMove(){
         console.log("AI turn starts")
-        let mov_arr =  this.findMovablePieces(this.stratego.chessBoardData,this.stratego.team1);
-        console.log("movable array"+mov_arr);
-        let des = this.makeGuesses(mov_arr,this.stratego.chessBoardData).origin;
+        let mov_arr =  this.findMovablePieces(this.stratego.chessBoardData,this.stratego.chessPieces.team1);
+        console.log("find movable array")
+        console.log(mov_arr);
+        let result = this.makeGuesses(mov_arr,this.stratego.chessBoardData);
+        console.log(result);
+        let des= result.origin;
         console.log("des"+des);
         let ox=des.pos.x;
         let oy=des.pos.y;
@@ -61,6 +64,7 @@ class AI{
     makeGuesses(movableArr,board){
         //  做一个 for loop for movable pieces
         // 每一个 可以移动的棋子检测 周围 的分数
+        console.log("enter make guesses")
         let minMax= movableArr;
         if (minMax == undefined)
             return  null;
@@ -107,48 +111,53 @@ class AI{
         }
     }
 
-    getAllMove(start,board) {
-        let row = start[0];
-        let col = start[1];
-        let mypieces = board[row][col];
-        let myRank = this.stratego.team1;
-    }
-
     /*寻找所有棋子，可移动棋子加入array */
     findMovablePieces(board,AI){
         let movable_pieces = new Array();
-        for (let y = 0 ; y < AI.length; y++){
-            let x_length = AI[y].length;
-            for(let x = 0 ; x < x_length.length; x++){
+        for (let x = 0 ; x < 10; x++){
+            for(let y = 0 ; y < 10; y++){
+                if(board[y][x]!=undefined&&board[y][x]!="water"&&board[y][x].team!=2){
+                    let temp={origin: board[x][y]}
+                    if(x>0){
+                        if(this.movablePieces(board[x-1][y],x-1,y)){
+                            temp.left=board[x-1][y];
+                        }else{
+                            temp.left=null ;
+                        }
+                    }
 
-                let temp={origin: board[x][y]}
-                if(this.movablePieces(board[x-1][y],x-1,y)){
-                    temp.left=board[x-1][y];
-                }else{
-                    temp.left=null ;
+                    if(x<9){
+                        if(this.movablePieces(board[x+1][y],x+1,y)){
+                            temp.right=board[x+1][y];
+                        }else{
+                            temp.right=null;
+                        }
+                    }
+
+
+                    if(y>0){
+                        if(this.movablePieces(board[x][y-1],x,y-1)){
+                            temp.above=board[x][y-1];
+                        }else{
+                            temp.above=null;
+                        }
+                    }
+
+                    if(y<9){
+                        if(this.movablePieces(board[x][y+1],x,y+1)){
+                            temp.below=board[x][y+1];
+                        }else{
+                            temp.below=null;
+                        }
+                    }
+
+
+
+                    if(temp.above!=null||temp.below!=null||temp.left!=null||temp.right!=null){
+                        movable_pieces.push(temp);
+                    }
                 }
 
-                if(this.movablePieces(board[x+1][y],x+1,y)){
-                    temp.right=board[x+1][y];
-                }else{
-                    temp.right=null;
-                }
-
-                if(this.movablePieces(board[x][y-1],x,y-1)){
-                    temp.above=board[x][y-1];
-                }else{
-                    temp.above=null;
-                }
-
-                if(this.movablePieces(board[x][y+1],x,y+1)){
-                    temp.below=board[x][y+1];
-                }else{
-                    temp.below=null;
-                }
-
-                if(temp.above!=null||temp.below!=null||temp.left!=null||temp.right!=null){
-                    movable_pieces.append(temp);
-                }
             }
         }
         return movable_pieces;
