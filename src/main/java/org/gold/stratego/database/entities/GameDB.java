df8916cc90ee8eff59e5c24053e5edea66a565ef
Jacob <jathoma98@gmail.com>
2019-09-27 16:49:41 -0400

    Implemented method to get currently active game.

--- src/main/java/org/gold/stratego/database/entities/GameDB.java
+++ src/main/java/org/gold/stratego/database/entities/GameDB.java
@@ -5,6 +5,10 @@ import org.gold.stratego.database.entities.Turn;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Criteria;
+
+import java.util.List;
 
 @Transactional
 @Component
@@ -12,6 +16,25 @@ public class GameDB{
 
     @Autowired
     public GameRepository repo;
+
+    /**
+     * Gets the currently active game for the given user.
+     * @param username
+     * @return Game with key-value: "finished" = "false"
+     *         Null if no active games
+     *         or
+     *         user is Anonymous
+     */
+    public Game getActiveGame(String username){
+        if (username.equals("Anonymous"))
+            return null;
+        List<Game> games = repo.findActive(username);
+        if (games.size() == 0)
+            return null;
+        //TODO: remove this after testing
+        System.out.println(games.toString());
+        return games.get(0);
+    }
 
 
 
